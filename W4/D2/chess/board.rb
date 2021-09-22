c7719e9193e3fa740ef53a5cfab1f0a580c280b5
Grayson Poon <grayson.a.poon@gmail.com>
2021-09-21 18:17:51 -0700

    Test bracket methods in board.rb

--- W4/D2/chess/board.rb
+++ W4/D2/chess/board.rb
@@ -9,12 +9,6 @@ class Board
   def initialize
     @rows = Array.new(8) { Array.new(8) }
 
-    # @rows.each.with_index do |row, i|
-    #   if [0, 1, 6, 7].include?(i)
-    #     row.map! { |slot| slot = Piece.new(:B, @rows, ) }
-    #   end
-    # end
-
     @rows.each.with_index do |row, row_i|
       if [0, 1].include?(row_i)
         row.each.with_index do |slot, col_i|
@@ -27,7 +21,6 @@ class Board
       end
     end
 
-
     @rows.each.with_index do |row, row_i|
       row.each.with_index do |slot, col_i|
         if slot == nil
@@ -37,23 +30,38 @@ class Board
     end
   end
 
+  def [](pos)
+    row, col = pos
+    @rows[row][col]
+  end
+
+  def []=(pos, val)
+    row, col = pos
+    @rows[row][col] = val
+  end
+
   def move_piece(color, start_pos, ending_pos)
-    if start_pos == nil 
+    if @rows[start_pos.first][start_pos.last].is_a?(NullPiece)
       raise "No starting piece here"
     end
 
-    if !rows.include?(ending_pos.first) || !rows.include?(ending_pos.last)
+    if !(0..7).to_a.include?(ending_pos.first) || !(0..7).to_a.include?(ending_pos.last)
       raise "Illegal move"
     end
 
     @rows[start_pos.first][start_pos.last], @rows[ending_pos.first][ending_pos.last] = @rows[ending_pos.first][ending_pos.last],  @rows[start_pos.first][start_pos.last]
-
-
+    # @rows[start_pos], @rows[ending_pos] = @rows[ending_pos], @rows[start_pos]
+  
   end
 
-  
 
 end
 
 b = Board.new
-p b
+
+p b.move_piece(:B, [0, 0], [5, 0])
+puts "----------"
+p b.[]([5,4])
+p b.[5,4]
+
+
--- W4/D2/chess/piece.rb
+++ W4/D2/chess/piece.rb
@@ -3,14 +3,14 @@ require_relative "null_piece"
 class Piece
   attr_reader :board, :color, :position
 
-  def initialize(color, position)
+  def initialize(color, position) # pass in self when initializing a piece with a board instance
     @color = color
     # @board = Board.new
     @position = position # [0, 0]
   end
 
   def inspect
-    "<RegularPiece>"
+    "<RegularPiece> #{color}"
   end
 
   # def []=(pos, self)
