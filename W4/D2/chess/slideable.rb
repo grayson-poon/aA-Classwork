1386ce09ff7e5e7b1d61e305d2c1809ffca88a89
Williamsliam23 <williams.liam23@gmail.com>
2021-09-22 16:50:06 -0500

    slideable constant arrays, grow_unblocked_moves

--- W4/D2/chess/slideable.rb
+++ W4/D2/chess/slideable.rb
@@ -1,11 +1,11 @@
 module Slideable
 
   def horizontal_dirs
-    
+    HORIZONTAL_DIRS
   end
 
   def diagnonal_dirs
-
+    DIAGONAL_DIRS
   end
 
   def moves
@@ -14,7 +14,22 @@ module Slideable
 
   private
 
-  HORIZONTAL_DIRS = []
-  DIAGONAL_DIRS = []
+  HORIZONTAL_DIRS = [[0, 1], [1, 0], [-1, 0], [0, -1]]
+  DIAGONAL_DIRS = [[1, 1], [1,-1], [-1, -1], [-1, 1]]
+
+  def grow_unblocked_moves
+    i = 1
+    possible_positions = []
+    HORIZONTAL_DIRS.each do |unit_dir|
+      extended_pos = unit_dir.map(&:*i)
+      if @board_instance[extended_pos] != NullPiece.instance
+        if @board_instance[extended_pos].color != self.color
+          possible_positions << extended_pos
+        end
+      else
+        possible_positions << extended_pos
+      end
+    end
+  end
 
 end
