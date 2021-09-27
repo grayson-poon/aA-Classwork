bc5ff7adae0caa586a2b31cf103df72c4b2e1d62
Grayson Poon <grayson.a.poon@gmail.com>
2021-09-27 15:38:24 -0700

    Initialize IntSet class

--- W5/hash_map_and_LRU_cache/lib/p01_int_set.rb
+++ W5/hash_map_and_LRU_cache/lib/p01_int_set.rb
@@ -2,25 +2,27 @@ require "set"
 
 class MaxIntSet
 
-  attr_reader :max
+  attr_reader :max, :store
 
   def initialize(max)
+    # @set = Set.new(0..max)
     @store = Array.new(max, false)
     @max = max
   end
 
   def insert(num)
     if num > max || num < 0
-      raise 'value_error' 
+      raise "Out of bounds" 
     else
-      self.add(num)
+      # @set.add(num)
       @store[num] = true
     end
 
   end
 
   def remove(num)
-    self.delete(num)
+    # @set.delete(num)
+    @store[num] = false
   end
 
   def include?(num)
@@ -29,13 +31,6 @@ class MaxIntSet
     else
       return false
     end
-
-
-    # @store.each do |ele|
-    #   return true if ele == num
-    # end
-
-    # return false
   end
 
   private
@@ -49,22 +44,33 @@ end
 
 
 class IntSet
+
+  attr_reader :store
+
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
   end
 
   def insert(num)
+    @store[num % num_buckets] << num
   end
 
   def remove(num)
+    @store[num].delete(num)
   end
 
   def include?(num)
+    if @store[num % num_buckets] == num
+      return true
+    else
+      return false
+    end
   end
 
   private
 
   def [](num)
+    # @store[num % num_buckets]
     # optional but useful; return the bucket corresponding to `num`
   end
 
