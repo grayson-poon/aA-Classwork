032f8d9e81fdac5fdac509e76cae698a95145e76
Grayson Poon <grayson.a.poon@gmail.com>
2021-09-30 17:03:50 -0700

    SCreate associations for Enrollment class

--- W5/D4/associations_exercise/app/models/enrollment.rb
+++ W5/D4/associations_exercise/app/models/enrollment.rb
@@ -1,2 +1,24 @@
+# == Schema Information
+#
+# Table name: enrollments
+#
+#  id         :bigint           not null, primary key
+#  course_id  :integer
+#  student_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Enrollment < ApplicationRecord
+
+  has_many :users,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :User
+
+  belongs_to :course,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Course
+
+
 end
