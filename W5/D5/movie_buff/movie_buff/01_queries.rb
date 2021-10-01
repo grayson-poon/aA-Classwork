8c156ffcf7eb2e35ce0023d187643ee8e852c2c6
Grayson Poon <grayson.a.poon@gmail.com>
2021-10-01 12:09:33 -0700

    Write Active Record queries

--- W5/D5/movie_buff/movie_buff/01_queries.rb
+++ W5/D5/movie_buff/movie_buff/01_queries.rb
@@ -7,6 +7,8 @@ def it_was_ok
   #
   # Find the id, title, and score of all movies with scores between 2 and 3
 
+  Movie.select(:id, :title, :score).where(score: 2..3)
+
 end
 
 def harrison_ford
@@ -20,6 +22,12 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford
   # appeared but not as a lead actor
+
+  Actor
+    .joins(:movies, :castings)
+    .where("actors.name = 'Harrison Ford' AND castings.ord != 1")
+    .select("movies.id, movies.title")
+    .distinct
 
 end
 
