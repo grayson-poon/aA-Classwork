4453d28043aad397c174c0bedd9dc8002c4d0c37
Grayson Poon <grayson.a.poon@gmail.com>
2021-10-05 19:11:43 -0700

    Metaprogramming exercises

--- W6/D2/build_your_own_active_record_part1/lib/01_sql_object.rb
+++ W6/D2/build_your_own_active_record_part1/lib/01_sql_object.rb
@@ -5,18 +5,36 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    all = DBConnection.execute2(<<-SQL).first
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+    SQL
+
+    all.map! { |str| str.to_sym }
   end
 
   def self.finalize!
+    self.columns.each do |col_name|
+      define_method(col_name) do
+        self.attributes[col_name]
+      end
+    end
+
+    self.columns.each do |col_name|
+      define_method("#{col_name}=") do |arg|
+        self.attributes[col_name] = arg
+      end
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    return "#{table_name.downcase}s"
   end
 
   def self.table_name
-    # ...
+    return "#{self.to_s.downcase}s"
   end
 
   def self.all
@@ -32,11 +50,14 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |name, val|
+      raise "unknown attribute '#{name}'" if !self.class.columns.include?(name)
+      self.send("#{name}=", val)
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
