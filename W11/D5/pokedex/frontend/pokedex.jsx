6d4118c69449bc5d0975b218c02fc75501e347f8
grayson-poon <grayson.a.poon@gmail.com>
2021-11-12 15:18:57 -0800

    set up store and update state

--- W11/D5/pokedex/frontend/pokedex.jsx
+++ W11/D5/pokedex/frontend/pokedex.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM  from "react-dom";
 import { Root } from "./components/root";
-import { receiveAllPokemon, RECEIVE_ALL_POKEMON } from "./actions/pokemon_actions"
+import { receiveAllPokemon } from "./actions/pokemon_actions"
 import fetchAllPokemon from "./util/api_util"
 import pokemonReducer from "./reducers/pokemon_reducer"
+import configureStore from "./store/store";
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(<Root />, document.getElementById("main"))
   window.receiveAllPokemon = receiveAllPokemon; 
   window.fetchAllPokemon = fetchAllPokemon; 
-  window.pokemonReducer = pokemonReducer; 
+  window.pokemonReducer = pokemonReducer;
+
+  const store = configureStore();
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 });
